fca82def0debc300b3deaa2889cdc336d469f4aa
nabilahunafaa <nabilahunnafa02@gmail.com>
2026-03-29 12:59:32 +0700

    update: footer layout

---
+++ resources/views/layouts/parts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="w-full bg-white border-t px-6 py-3 text-sm text-gray-500 flex justify-between">
+    <span>&copy; 2026 MidSemester6Project</span>
+    <span>Kelompok 1</span>
+</footer>
